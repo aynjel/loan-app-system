89cf2ee1ea3a1df9c62afc3726cfe7261b6a7266
aynjel <ortegacanillo76@gmail.com>
2023-08-17 15:55:50 +0800

    adding reactjs as frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,21 @@
+import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+function App() {
+
+  return (
+    <>
+      <div className="card">
+        <div className="card-body">
+          <h5 className="card-title">Card title</h5>
+          <h6 className="card-subtitle mb-2 text-muted">Card subtitle</h6>
+          <p className="card-text"></p>
+          <a href="#" className="card-link">Card link</a>
+          <a href="#" className="card-link">Another link</a>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
